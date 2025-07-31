3ce8b626b531080fa5465a6aacc01c3dc0bb3ee1
ParkJH <qwg531@naver.com>
2025-07-31 10:47:53 +0900

    chore: 메인실행 파일 들여쓰기 수정

--- src/main/java/com/example/yenanow/YenanowApplication.java
+++ src/main/java/com/example/yenanow/YenanowApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class YenanowApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(YenanowApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(YenanowApplication.class, args);
+    }
 }
