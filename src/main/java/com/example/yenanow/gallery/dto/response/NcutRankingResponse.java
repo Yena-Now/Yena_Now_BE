9870852ee7ff7cbdd66992ea21e5b4542a5602b5
qwg531 <qwg531@naver.com>
2025-08-13 00:36:07 +0900

    fix: response [key 반환 -> Url 반환]으로 변경
    - 2025.08.13 진행
    
    S13P11E203-232 #done 완료

--- src/main/java/com/example/yenanow/gallery/dto/response/NcutRankingResponse.java
+++ src/main/java/com/example/yenanow/gallery/dto/response/NcutRankingResponse.java
@@ -1,6 +1,7 @@
 package com.example.yenanow.gallery.dto.response;
 
 import com.example.yenanow.gallery.entity.Ncut;
+import com.example.yenanow.s3.service.S3Service;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,10 +17,10 @@ public class NcutRankingResponse {
     private String ncutUrl;
     private int likeCount;
 
-    public static NcutRankingResponse fromEntity(Ncut ncut) {
+    public static NcutRankingResponse fromEntity(Ncut ncut, S3Service s3Service) {
         return NcutRankingResponse.builder()
             .ncutUuid(ncut.getNcutUuid())
-            .ncutUrl(ncut.getNcutUrl())
+            .ncutUrl(s3Service.getFileUrl(ncut.getNcutUrl()))
             .likeCount(ncut.getLikeCount())
             .build();
     }
